4a17bc0aeed7cc87a946876f1aefb0eaecd561fa
liuxxxxxxxxx <1038510224@qq.com>
2021-12-26 15:28:45 +0800

    组合回溯

--- src/main/java/com/lydia/algorithm/backtracking/组合.java
+++ src/main/java/com/lydia/algorithm/backtracking/组合.java
@@ -1,18 +1,42 @@
 package com.lydia.algorithm.backtracking;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
 public class 组合 {
 
     public static void main(String[] args) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        Deque<Integer> path = new LinkedList<>();
+
+        backTracking(path, res, 4, 2, 1);
+        System.out.println(res);
 
     }
 
 
     /**
-     * 给定一个数组[1,2,3,4],取出target个数组的子集
-     *
-     *
+     * 给定一个n,k,取出[1...n]数量为k的数组，
+     * 4,2
+     * [1,2,3,4]
+     *          取1          取2          取3          取4
+     * 取2  取3  取4      取3  取4      取4            null
+     * [1,2][1,3][1,4]   [2,3][2,4]    [3,4]
      */
-    private static void backTracking(){
+    private static void backTracking(Deque<Integer> path, List<List<Integer>> res, int n, int k, int startIndex) {
+        if (path.size() == k) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = startIndex; i <= n; i++) {
+            path.addLast(i);
+            backTracking(path, res, n, k, i+1);
+            path.removeLast();
+        }
 
     }
 }
